850d586a9b3cd543267e650716033d659e355c29
Jue <me@jue.yt>
2019-09-12 11:24:25 +0200

    Made pipe view more readable

--- ui/components/app.js
+++ ui/components/app.js
@@ -1,6 +1,6 @@
 import {LitElement, html} from '@polymer/lit-element';
 import '@vaadin/vaadin-grid';
-import '@vaadin/vaadin-grid/vaadin-grid-sort-column';
+import '@vaadin/vaadin-grid/vaadin-grid-filter-column';
 
 class MyApp extends LitElement {
 	static get properties () {
@@ -19,13 +19,24 @@ class MyApp extends LitElement {
 			const socket = io.connect();
 			socket.on('pipe', (p) => {
 				p.timestamp = new Date(p.timestamp).toISOString();
+				if (typeof p.value === 'number') {
+					p.value = p.value.toFixed(2);
+				} else if (p.value === undefined) {
+					p.value = 'undefined'
+				} else if (typeof p.value === 'object') {
+					p.value = JSON.stringify(p.value);
+				}
 				let found = false;
 				const items = this.items.map((i) => {
 					if (i.pipe !== p.pipe) return i;
 					found = true;
 					return p;
 				});
-				this.items = (found) ? items : [...items, p];
+				this.items = (found) ? items : [...items, p].sort((a, b) => {
+					if (a.pipe < b.pipe) return -1;
+					if (a.pipe > b.pipe) return 1;
+					return 0;
+				});
 			});
 		};
 		this.items = [];
@@ -39,9 +50,9 @@ class MyApp extends LitElement {
 				}
 			</style>
 			<vaadin-grid .items="${this.items}">
-				<vaadin-grid-sort-column path="timestamp" header="Received" resizable></vaadin-grid-sort-column>
-				<vaadin-grid-sort-column path="pipe" header="Pipe" resizable></vaadin-grid-sort-column>
-				<vaadin-grid-column path="value" header="Value" resizable></vaadin-grid-column>
+				<vaadin-grid-column path="timestamp" header="Received" resizable .flexGrow="${0}" .width="${'250px'}"></vaadin-grid-column>
+				<vaadin-grid-filter-column path="pipe" header="Pipe" resizable .flexGrow="${3}"></vaadin-grid-filter-column>
+				<vaadin-grid-column path="value" header="Value" .textAlign="${'end'}" resizable .flexGrow="${1}"></vaadin-grid-column>
 			</vaadin-grid>
 		`;
 	}
